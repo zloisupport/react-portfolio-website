99bbd45c792bce30416eae1c99357202bbeace97
zloisupport <kr0t77@yandex.ru>
2022-05-01 04:34:56 +0600

    +header and cta jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,27 @@
 ﻿import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/Me.gif'
+import HeaderSocial from './HeaderSocial'
 const Header = () => {
   return (
-    <div>Header</div>
+   <header>
+     <div className="container header__container">
+       <h5>Привет я</h5>
+       <h1> Zloisupport</h1>
+       <h5 className="text-light">Junior Developer</h5>
+
+        <CTA/>
+        <HeaderSocial/>
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+  
+        
+        <a href='#contact' className='scroll__down'>Scroll Down</a>
+
+     </div>
+   </header>
   )
 }
 
---
+++ src/components/header/HeaderSocial.jsx
@@ -0,0 +1,16 @@
+﻿import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {DiGithubBadge} from 'react-icons/di'
+import {FaDribbble} from 'react-icons/fa'
+const HeaderSocial = () => {
+  return (
+   <div className="header__socials">
+       <a href='https://likedn' target="_blank"><BsLinkedin/></a>
+       <a href='https://github.com' target="_blank" title='Мой github'><DiGithubBadge/></a>
+       <a href='https://likedn' target="_blank"><FaDribbble/></a>
+
+   </div>
+  )
+}
+ 
+export default HeaderSocial
