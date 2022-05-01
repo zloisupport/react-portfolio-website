f2143f4b75da24804ed683471d76dbd395c4f861
zloisupport <kr0t77@yandex.ru>
2022-05-02 01:58:56 +0600

    footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,29 @@
 ﻿import React from 'react'
+import {AiFillTwitterCircle} from 'react-icons/ai'
 import './footer.css'
 
  
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>Name</a>
+      <ul className='permalinks'>
+        <li><a href="#"></a>Home</li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#services">services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+      </ul>
+
+
+      <div className="footer__socials">
+        <a href="http://">VK</a>
+        <a href="http://"><AiFillTwitterCircle/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; All rights reserved</small>
+      </div>
+    </footer>
   )
 }
 
