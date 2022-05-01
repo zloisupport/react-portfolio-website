acc6786ce5d6d8b5d762a8cb393ae32f241c2374
zloisupport <kr0t77@yandex.ru>
2022-05-01 21:35:31 +0600

    ~ services

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,89 @@
 ﻿import React from 'react'
+import {BiListCheck} from 'react-icons/bi'
 import './services.css'
 const Services = () => {
   return (
-    <div>Services</div>
+    <section id='services'>
+      <h5>What I offer</h5>
+      <h2>Services</h2>
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX Design</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BiListCheck className='service__list-icon'/>
+              <p> Sunt aliquip mollit commodo officia. Dolor anim aliquip aliquip sint quis velit. Lorem occaecat id quis ex fugiat eiusmod enim.</p>
+            </li>           
+             <li>
+              <BiListCheck className='service__list-icon'/>
+              <p> Sunt aliquip mollit commodo officia. Dolor anim aliquip aliquip sint quis velit. Lorem occaecat id quis ex fugiat eiusmod enim.</p>
+            </li>           
+             <li>
+              <BiListCheck className='service__list-icon'/>
+              <p> Sunt aliquip mollit commodo officia. Dolor anim aliquip aliquip sint quis velit. Lorem occaecat id quis ex fugiat eiusmod enim.</p>
+            </li>           
+             <li>
+              <BiListCheck className='service__list-icon'/>
+              <p> Sunt aliquip mollit commodo officia. Dolor anim aliquip aliquip sint quis velit. Lorem occaecat id quis ex fugiat eiusmod enim.</p>
+            </li>
+          </ul>
+        </article>
+
+        <article className="service">
+          <div className="service__head">
+            <h3>Web development</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BiListCheck className='service__list-icon'/>
+              <p> Sunt aliquip mollit commodo officia. Dolor anim aliquip aliquip sint quis velit. Lorem occaecat id quis ex fugiat eiusmod enim.</p>
+            </li>           
+             <li>
+              <BiListCheck className='service__list-icon'/>
+              <p> Sunt aliquip mollit commodo officia. Dolor anim aliquip aliquip sint quis velit. Lorem occaecat id quis ex fugiat eiusmod enim.</p>
+            </li>           
+             <li>
+              <BiListCheck className='service__list-icon'/>
+              <p> Sunt aliquip mollit commodo officia. Dolor anim aliquip aliquip sint quis velit. Lorem occaecat id quis ex fugiat eiusmod enim.</p>
+            </li>           
+             <li>
+              <BiListCheck className='service__list-icon'/>
+              <p> Sunt aliquip mollit commodo officia. Dolor anim aliquip aliquip sint quis velit. Lorem occaecat id quis ex fugiat eiusmod enim.</p>
+            </li>
+          </ul>
+        </article>
+
+        
+        <article className="service">
+          <div className="service__head">
+            <h3>Web development</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BiListCheck className='service__list-icon'/>
+              <p> Sunt aliquip mollit commodo officia. Dolor anim aliquip aliquip sint quis velit. Lorem occaecat id quis ex fugiat eiusmod enim.</p>
+            </li>           
+             <li>
+              <BiListCheck className='service__list-icon'/>
+              <p> Sunt aliquip mollit commodo officia. Dolor anim aliquip aliquip sint quis velit. Lorem occaecat id quis ex fugiat eiusmod enim.</p>
+            </li>           
+             <li>
+              <BiListCheck className='service__list-icon'/>
+              <p> Sunt aliquip mollit commodo officia. Dolor anim aliquip aliquip sint quis velit. Lorem occaecat id quis ex fugiat eiusmod enim.</p>
+            </li>           
+             <li>
+              <BiListCheck className='service__list-icon'/>
+              <p> Sunt aliquip mollit commodo officia. Dolor anim aliquip aliquip sint quis velit. Lorem occaecat id quis ex fugiat eiusmod enim.</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
   )
 }
 
